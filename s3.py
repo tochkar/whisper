266c80c895e0e4b4c7ccbb18419da46aacb67549
tochkar <55049700+tochkar@users.noreply.github.com>
2024-11-11 22:57:59 +0300

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,12 @@
+import csv
+import os
+import json
+import boto3
+from dotenv import load_dotenv
+
+bucket = os.environ['BUCKET']
+s3 = boto3.client('s3', region_name='eu-west-1', aws_access_key_id=os.environ['ACCESS_KEY'],
+                  aws_secret_access_key=os.environ['SECRET_KEY'], aws_session_token=os.environ['SESSION_TOKEN'])
+
+
+s3.download_file(bucket, 'taxi135_public_Orders-08.2024.csv', 'rows')
